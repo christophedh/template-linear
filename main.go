e41612404042b194159a12822eef848f43de2c9b
christophe dhayer <christophe.dhayer@gmail.com>
2020-08-03 14:10:51 -0400

    setup port for heroku

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("./public"))))
+	port := getPORT()
+	fmt.Printf("serve on %s\n", port)
+	log.Fatal(http.ListenAndServe(port, http.FileServer(http.Dir("./public"))))
+}
+
+func getPORT() string {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+	port = ":" + port
+	return port
 }
